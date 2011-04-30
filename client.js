2a7bda9c574dcc9938e84e23f48ff3cd0fc0b8ff
Brent Daugherty <brent@doorty.com>
2011-04-29 18:41:15 -0700

    first step towards object oriented piece design

--- client.js
+++ client.js
@@ -1,12 +1,104 @@
-window.addEventListener('load', function() { 
+var game = function() { 
   
   /**************************************************************************
-    * init
-    **************************************************************************/
-   var canvas = document.getElementById('canvas');
-   context = canvas.getContext('2d');
-   window.addEventListener('devicemotion', deviceMotion, true);
+  * variables 
+  **************************************************************************/
+  var pieces = [];
+
+  var board = {
+    WIDTH: 320,
+    HEIGHT: 460
+  };
   
+  var BoardPiece = function(sessionId) {
+    this.sessionId = sessionId;
+    this.center = { 
+      x: board.WIDTH / 2,
+      y: 0
+    };
+    this.radius = 32;
+    this.color = '#000';
+  };
+  
+  var piece = new BoardPiece(3);
+
+  /*
+  var piece = {
+  	center: {
+  		x: board.WIDTH / 2,
+  		y: 0,
+  		xShift: 0,
+  		yShift: 0
+  	},
+  	radius: 32,
+  	color: '#000'
+  };
+  */
+  
+  /**************************************************************************
+  * functions 
+  **************************************************************************/
+     
+  
+   
+  var deviceMotion = function(event) {
+    var accel = event.accelerationIncludingGravity;
+    drawBoard();
+    piece.center = computeCenter(piece.center, accel);
+    drawPiece(piece);
+  };
+
+  var drawBoard = function() {
+     context.clearRect(0, 0, board.WIDTH, board.HEIGHT);
+     for (var x = 0.5; x < board.WIDTH; x += 10) {
+         context.moveTo(x, 0);
+         context.lineTo(x, board.HEIGHT);
+     }
+     for (var y = 0.5; y < board.HEIGHT; y += 10) {
+        context.moveTo(0, y);
+        context.lineTo(board.WIDTH, y);
+     }
+     context.strokeStyle = "#eee";
+     context.stroke();
+  };
+
+  var drawPiece = function(p) {
+     context.fillStyle = p.color;
+     context.beginPath();
+     context.arc(p.center.x, p.center.y, p.radius, 0, Math.PI * 2, false);
+     context.closePath();
+     context.fill();
+  };
+
+  var computeCenter = function(oldCenter, acceleration) {
+     var newCenter = {};
+
+     newCenter.x = oldCenter.x + 2 * acceleration.x;
+     newCenter.y = oldCenter.y - 2 * acceleration.y;
+     
+     // stay within canvas boundary
+     if (newCenter.x < piece.radius) {
+        newCenter.x = piece.radius;
+     }
+     if (newCenter.x > board.WIDTH - piece.radius) {
+        newCenter.x = board.HEIGHT - piece.radius;
+     }
+     if (newCenter.y < piece.radius) {
+        newCenter.y = piece.radius;
+     }
+     if (newCenter.y > board.HEIGHT - piece.radius) {
+        newCenter.y = board.HEIGHT - piece.radius;
+     }
+     return newCenter;
+  };
+  
+  /**************************************************************************
+  * init
+  **************************************************************************/
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  window.addEventListener('devicemotion', deviceMotion, true);
+
   /**************************************************************************
    * socket connection with server. 
    * iOS > 4.2 uses WebSocket, iOS < 4.2 uses long polling
@@ -23,80 +115,7 @@ window.addEventListener('load', function() {
     console.log('disconnected');
   });
 
-});
-
-/**************************************************************************
- * variables 
- **************************************************************************/
-var context;
-
-var board = {
-  width: 320,
-  height: 460
 };
 
-var piece = {
-	center: {
-		x: board.width / 2,
-		y: 0,
-		xShift: 0,
-		yShift: 0
-	},
-	radius: 32,
-	color: '#000'
-};
+window.addEventListener('load', game);
 
-/**************************************************************************
- * functions 
- **************************************************************************/
-var deviceMotion = function(event) {
-  var accel = event.accelerationIncludingGravity;
-  drawBoard();
-  piece.center = computeCenter(piece.center, accel);
-  drawPiece(piece);
-}
-
-function drawBoard() {
-   context.clearRect(0, 0, board.width, board.height);
-   for (var x = 0.5; x < board.width; x += 10) {
-       context.moveTo(x, 0);
-       context.lineTo(x, board.height);
-   }
-   for (var y = 0.5; y < board.height; y += 10) {
-      context.moveTo(0, y);
-      context.lineTo(board.width, y);
-   }
-   context.strokeStyle = "#eee";
-   context.stroke();
-}
-
-function drawPiece(piece) {
-   context.fillStyle = piece.color;
-   context.beginPath();
-   context.arc(piece.center.x, piece.center.y, piece.radius, 0, Math.PI * 2, false);
-   context.closePath();
-   context.fill();
-}
-
-function computeCenter (oldCenter, acceleration) {
-   newCenter = {};
-   newCenter.xShift = oldCenter.xShift * 0.8 + acceleration.x * 2.0;
-   newCenter.yShift = oldCenter.yShift * 0.8 + acceleration.y * 2.0;
-   newCenter.x = oldCenter.x + oldCenter.xShift;
-   // use *minus* to compute the center's new y
-   newCenter.y = oldCenter.y - oldCenter.yShift;
-   // do not go outside the boundaries of the canvas
-   if (newCenter.x < piece.radius) {
-      newCenter.x = piece.radius;
-   }
-   if (newCenter.x > board.width - piece.radius) {
-      newCenter.x = board.width - piece.radius;
-   }
-   if (newCenter.y < piece.radius) {
-      newCenter.y = piece.radius;
-   }
-   if (newCenter.y > board.height - piece.radius) {
-      newCenter.y = board.height - piece.radius;
-   }
-   return newCenter;
-}
